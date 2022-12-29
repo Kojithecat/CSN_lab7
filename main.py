00731fb5b461bd59cd1475237ef4d6960a67f936
pierreachkar1717 <Dj\piero@1717!>
2022-12-29 15:02:31 +0100

    minor changes & Task 2

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def update_status(G, infected_nodes, beta, gamma):
     # Return the list of infected nodes and the proportion of infected nodes
     return new_infected_nodes, len(new_infected_nodes) / len(G)
 
-def simulate_spread(G, infected_nodes, beta, gamma, T):
+def simulate_spread(G, G_type, infected_nodes, beta, gamma, T):
     """
         Simulate the spread of a disease on a graph and plot the proportion of infected nodes over time.
 
@@ -80,6 +80,8 @@ def simulate_spread(G, infected_nodes, beta, gamma, T):
         ----------
         G : networkx.Graph
             The graph to simulate the spread of the disease on.
+        G_type : str
+            The type of graph.
         infected_nodes : list of int
             A list of the nodes that are infected at time t-1.
         beta : float
@@ -106,13 +108,16 @@ def simulate_spread(G, infected_nodes, beta, gamma, T):
         proportion_infected.append(p)
 
     # Plot the proportion of infected nodes over time
-    plt.plot(proportion_infected)
-    plt.xlabel('Time step')
-    plt.ylabel('Proportion of infected nodes')
-    plt.show()
+    # plt.plot(proportion_infected)
+    # plt.xlabel('Time step')
+    # plt.ylabel('Proportion of infected nodes')
+    # plt.show()
 
     # Calculate the eigenvalue centrality of each node
-    eigen_centrality = nx.eigenvector_centrality(G)
+    if G_type == 'lattice' or G_type == 'star':
+        eigen_centrality = nx.eigenvector_centrality(G, max_iter=600)
+    else:
+        eigen_centrality = nx.eigenvector_centrality(G)
 
     # Find the highest eigenvalue centrality value
     max_eigen = max(eigen_centrality.values())
@@ -146,46 +151,106 @@ def check_threshold(beta, gamma, max_eigen):
     # Calculate Threshold
     threshold = 1 / max_eigen
 
+    print('beta:', beta)
+    print('gamma:', gamma)
+    print('beta/gamma:', beta_over_gamma)
+
     # Check whether the epidemic threshold theorem holds
-    if beta_over_gamma > 1 / threshold:
+    if beta_over_gamma > threshold:
         print('Epidemic occurs')
     else:
         print('No epidemic occurs')
 
 
 def main():
+    # #Task 1
+    # # Set the parameters
+    # p0 = 0.01
+    # beta = 0.1
+    # gamma = 0.6
+    # T = 10
+    # n = 1000  # number of vertices
+    #
+    # # List of graph types
+    # graph_types = ["random", "complete", "star", "lattice"]
+    #
+    # for graph_type in graph_types:
+    #     if graph_type == "random":
+    #         G = nx.fast_gnp_random_graph(n, 0.1)
+    #     elif graph_type == "complete":
+    #         G = nx.complete_graph(n)
+    #     elif graph_type == "star":
+    #         G = nx.star_graph(n)
+    #     elif graph_type == "lattice":
+    #         G = nx.grid_2d_graph(int(math.sqrt(n)), int(math.sqrt(n)))
+    #
+    #     # Set the initial infected nodes
+    #     infected_nodes = set_initial_infected(G, p0)
+    #
+    #     # Update the status of the nodes
+    #     update_status(G, infected_nodes, beta, gamma)
+    #
+    #     # Simulate the spread of the disease
+    #     proportion_infected, max_eigen, leading_node = simulate_spread(G, graph_type, infected_nodes, beta, gamma, T)
+    #
+    #     # Print the leading node
+    #     print("Graph type:", graph_type)
+    #     print("Leading node:", leading_node, "with eigenvalue centrality:", max_eigen)
+    #
+    #     # Check whether the epidemic threshold theorem holds
+    #     check_threshold(beta, gamma, max_eigen)
+
+    # Taks 2: two sets of beta and gamma, one slightly above the threshold and one slightly below the threshold
     # Set the parameters
     p0 = 0.01
-    beta = 0.001
-    gamma = 0.1
     T = 10
-    n = 1000 # number of vertices
+    n = 1000  # number of vertices
 
-    # Erdos-Renyi graph
-    #G = nx.fast_gnp_random_graph(n, 0.1)
-    # Complete graph
-    G = nx.complete_graph(n)
-    # Star graph
-    #G = nx.star_graph(n)
-    # 2D Lattice graph
-    #G = nx.grid_2d_graph(int(math.sqrt(n)), int(math.sqrt(n)))
-    print(G)
+    # List of graph types
+    graph_types = ["random", "complete", "star", "lattice"]
 
+    for graph_type in graph_types:
+        if graph_type == "random":
+            G = nx.fast_gnp_random_graph(n, 0.1)
+            threshold = 1 / 0.04097554219168759
+        elif graph_type == "complete":
+            G = nx.complete_graph(n)
+            threshold = 1 / 0.0316227766016838
+        elif graph_type == "star":
+            G = nx.star_graph(n)
+            threshold = 1 / 0.7070926221900043
+        elif graph_type == "lattice":
+            G = nx.grid_2d_graph(int(math.sqrt(n)), int(math.sqrt(n)))
+            threshold = 1 / 0.062295051731419034
 
-    # Set the initial infected nodes
-    infected_nodes = set_initial_infected(G, p0)
+        # Set the beta and gamma values slightly above and below the threshold
+        beta_above = 1.1 * threshold
+        gamma_above = threshold
 
-    # Update_status(G, infected_nodes, beta, gamma)
-    update_status(G, infected_nodes, beta, gamma)
+        beta_below = 0.9 * threshold
+        gamma_below = threshold
 
-    # Simulate the spread of the disease
-    proportion_infected, max_eigen, leading_node = simulate_spread(G, infected_nodes, beta, gamma, T)
+        # Set the initial infected nodes
+        infected_nodes = set_initial_infected(G, p0)
 
-    # Print the leading node
-    print('Leading node:', leading_node, 'with eigenvalue centrality:', max_eigen)
+        # Simulate the spread of the disease using the beta and gamma values above the threshold
+        proportion_infected_above, max_eigen_above, leading_node_above = simulate_spread(G, graph_type,
+                                                                                         infected_nodes, beta_above,
+                                                                                         gamma_above, T)
 
-    # Check whether the epidemic threshold theorem holds
-    check_threshold(beta, gamma, max_eigen)
+        # Simulate the spread of the disease using the beta and gamma values below the threshold
+        proportion_infected_below, max_eigen_below, leading_node_below = simulate_spread(G, graph_type,
+                                                                                         infected_nodes, beta_below,
+                                                                                         gamma_below, T)
+
+        # Print the leading nodes and proportion infected for each simulation
+        print("Graph type:", graph_type)
+        print("Threshold:", threshold)
+        print("Proportion infected above threshold:", proportion_infected_above[-1])
+        check_threshold(beta_above, gamma_above, max_eigen_above)
+        print("Proportion infected below threshold:", proportion_infected_below[-1])
+        check_threshold(beta_below, gamma_below, max_eigen_below)
+        print('##############################################')
 
 if __name__ == '__main__':
     main()
